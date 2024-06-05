e75bafc5cef471529bbcf8d700e565f3742418e0
ariefprasetyohadi <152331929+ariefprasetyohadi@users.noreply.github.com>
2024-06-06 05:08:21 +0700

    Add files via upload

---
+++ luas lingkaran.py
@@ -0,0 +1,7 @@
+#Membuat fungsi Menghitung luas lingkaran
+#Memanggil fungsi
+def hitung_luas_lingakaran(r):
+    luas=3.14 * r**2
+    return luas
+vluas = hitung_luas_lingakaran(10)
+print ("luas lingkaran adalah= ",vluas)
